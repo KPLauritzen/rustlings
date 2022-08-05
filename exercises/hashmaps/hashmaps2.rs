25a8dbc77492916313edd834b168adcef653c7a7
Kasper Primdal Lauritzen <git@kplauritzen.dk>
2022-08-05 17:09:25 +0200

    hashmaps

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -11,8 +11,6 @@
 //
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Hash, PartialEq, Eq)]
@@ -34,9 +32,12 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     ];
 
     for fruit in fruit_kinds {
-        // TODO: Put new fruits if not already present. Note that you
-        // are not allowed to put any type of fruit that's already
-        // present!
+        match basket.get(&fruit) {
+            Some(_) => {},
+            None => {
+                basket.insert(fruit, 10);
+            },
+        }
     }
 }
 
@@ -49,6 +50,7 @@ mod tests {
         basket.insert(Fruit::Apple, 4);
         basket.insert(Fruit::Mango, 2);
         basket.insert(Fruit::Lychee, 5);
+
 
         basket
     }
--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
@@ -35,14 +33,28 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
+
+        update_team(team_1_name, team_1_score, team_2_score, &mut scores);
+        update_team(team_2_name, team_2_score, team_1_score, &mut scores);
+
         // TODO: Populate the scores table with details extracted from the
         // current line. Keep in mind that goals scored by team_1
         // will be number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
-    }
+    };
     scores
 }
+fn update_team(name: String, goals: u8, conceded: u8, scores: &mut HashMap<String, Team>){
+    let team = scores.entry((&name).to_owned()).or_insert(Team {
+        name: name,
+        goals_scored: 0,
+        goals_conceded: 0
+    });
+    team.goals_scored += goals;
+    team.goals_conceded += conceded;
+}
+
 
 #[cfg(test)]
 mod tests {
